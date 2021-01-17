561c6c5f2d7ab9d80aaadeee6dd29011c353bb78
Alejogb1 <magnicetio@gmail.com>
2021-01-17 14:43:29 -0300

    Update Character.js

--- myapp/src/Components/Characters/Character.js
+++ myapp/src/Components/Characters/Character.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
-export default function Character() {
+import {Col, Card} from "react-bootstrap"
+export default function Character({name, image, gender}) {
     return (
         /*  */
-        <div>
-            <h4>HOLA</h4>
-        </div>
+        <Col md={4}>
+            <Card>
+                <h4>{name}</h4>
+            </Card>
+            <Card.Image variant="ontop" src={image}></Card.Image>
+        </Col>
     )
 }
