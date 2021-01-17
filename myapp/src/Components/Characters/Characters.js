1a6fffb4661f6d8c7e729da10c69baee3981afdb
Alejogb1 <magnicetio@gmail.com>
2021-01-17 14:43:25 -0300

    Update Characters.js

--- myapp/src/Components/Characters/Characters.js
+++ myapp/src/Components/Characters/Characters.js
@@ -3,21 +3,40 @@
 
 import {useState, useEffect} from 'react'
 import Character from "./Character"
+import {Row} from "react-bootstrap"
 const Characters = () => {
-    const BASE_URL = "https://rickandmortyapi.com/api/";
+        const BASE_URL = "https://rickandmortyapi.com/api";
         [characters, setCharacters] = useState([])    
+        [fetching, setFetch] = useState([true])    
+        [error, setError] = useState([false]) 
         const getCharacters = async (endpoint) => {
-        const result = await fetch (`${BASE_URL}`, `${endpoint}`)
-        const data = await result.json 
+
+            try {
+                const result = await fetch (`${BASE_URL}/${endpoint}`)
+                const data = await result.json
+                setCharacters(data.results) 
+                setFetch(false)
+            } catch (e) {
+                setError(true)
+                setFetch(true)
+                setCharacters([])
+            }
+    
        }
     useEffect(() => {
       getCharacters("character")
     }, [])
     
     return (
-        <>
-            <Character key={character.id}/>
-        </>
+        <Row>
+            {
+            fetching // si fetching es true..
+            ? <h3>Cargando...</h3>
+            : characters.map((character) => {
+                <Character key={character.id} {...character} /* Descompongo los valores *//> 
+            })      // characters es el array de objetos    
+            }
+        </Row>
     )
 }
 export default Characters;
