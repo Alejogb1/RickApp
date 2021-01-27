766fc8fe3dbf4025a0ca1b8d9deec10f4c94b8f7
Alejogb1 <alejogarciag1@gmail.com>
2021-01-27 11:59:26 -0300

    handleNexy y condicional para prev==null
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- myapp/src/Components/Paginate.js
+++ myapp/src/Components/Paginate.js
@@ -1,14 +1,20 @@
 import {Row, Col, Pagination} from "react-bootstrap"
 
-const Paginate = () => {
+const Paginate = ({prev, next, handlePages}) => {
+    const handleNext = () => { 
+        const [_, query] = next.split("?")
+        handlePages(query)
+    }
+    return (
     <Row>
         <Col>
             <Pagination>
-                <Pagination.Prev/>
-                <Pagination.Next/>
+                {!!prev && <Pagination.Prev/>} 
+                <Pagination.Next onClick={handleNext}/>
             </Pagination>
         </Col>  
     </Row>
+    ) 
 }
 
 export default Paginate
