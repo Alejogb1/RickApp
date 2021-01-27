758e4d4a2321f8e082c550e1577c7cb9b6681750
Alejogb1 <alejogarciag1@gmail.com>
2021-01-27 11:34:22 -0300

    Create Paginate.js
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

---
+++ myapp/src/Components/Paginate.js
@@ -0,0 +1,14 @@
+import {Row, Col, Pagination} from "react-bootstrap"
+
+const Paginate = () => {
+    <Row>
+        <Col>
+            <Pagination>
+                <Pagination.Prev/>
+                <Pagination.Next/>
+            </Pagination>
+        </Col>  
+    </Row>
+}
+
+export default Paginate
