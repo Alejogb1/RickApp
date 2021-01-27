ad2a68b32cc2fa55dd4ce7ed1bf9375c48b7c36e
Alejogb1 <alejogarciag1@gmail.com>
2021-01-27 20:29:01 -0300

    state for search
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import {Container} from "react-bootstrap"
 import  Characters from "./Components/Characters/index"
+import {useState} from "react"
+import Search from "./Components/Search/index"
 function App() {
+
+  const [search, setSearch] = useState("");
+
+  const handlerSearch = (name) => {
+    setSearch(name)
+  }
+
   return (
     <Container>
-      <Characters/>
+      <Search handlerSearch={handlerSearch}/>
+      <Characters search={search}/>
     </Container>
   );
 }
