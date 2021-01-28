d3eff6c1a3e281d08e74b1f9c0209013dd50fa4d
Alejogb1 <alejogarciag1@gmail.com>
2021-01-28 13:48:35 -0300

    Update Login.js
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- myapp/src/Components/Pages/Login/Login.js
+++ myapp/src/Components/Pages/Login/Login.js
@@ -1,7 +1,36 @@
+import {Row, Col, Form, Button} from "react-bootstrap"
+import {useForm} from "react-hook-form"
 const Login = () =>  {
+
+    const {register, handleSubmit, errors} = useForm();     // No se accede por vector, sino que por nombre
+
     return (
         <>
-            <h3>Login</h3>
+            <Row className="justify-content-center vh-100 align-items-center m-0 p-0">
+            <Col md={3} sm={12}>
+                <Form >
+                <Form.Group>
+                    <Form.Control
+                    type="text"
+                    name="username"
+                    />
+                    <label className="text-danger"></label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Control
+                    type="password"
+            
+                    name="password"
+                    
+                    placeholder="password"
+                    />
+                </Form.Group>
+                <Button type="submit" variant="dark" className="mt-3 w-100">
+                    Ingresar
+                </Button>
+                </Form>
+            </Col>
+            </Row>
         </>
     )
 }
