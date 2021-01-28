2a6b896bc232264490b923a1d5832ca3eaa6f24e
Alejogb1 <alejogarciag1@gmail.com>
2021-01-28 13:33:56 -0300

    Create Nav.js
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

---
+++ myapp/src/Components/Common/Nav.js
@@ -0,0 +1,14 @@
+
+
+const Nav = () => {
+    return (
+        <>  
+            <div>
+                <a href="/register">Register</a>
+                <a href="/login">Login</a>
+            </div>
+        </>
+    )
+}
+
+export default Nav
