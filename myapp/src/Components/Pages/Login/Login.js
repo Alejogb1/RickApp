1976579be477379ae1f09ab21904e4c027b93fa3
Alejogb1 <alejogarciag1@gmail.com>
2021-02-01 13:11:39 -0300

    schema and usePost
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- myapp/src/Components/Pages/Login/Login.js
+++ myapp/src/Components/Pages/Login/Login.js
@@ -1,25 +1,38 @@
 import {Row, Col, Form, Button} from "react-bootstrap"
 import {useForm} from "react-hook-form"
+import {schema} from "./schema"
+import { yupResolver } from "@hookform/resolvers"
 const Login = () =>  {
+ // No se accede por vector, sino que por nombre
+    const {register, handleSubmit, errors} = useForm({ 
+        resolver: yupResolver(schema),
+    });    
+    const submitForm = (data) => {
+        console.log(data) // Tengo data, en vez de e.target.value
+        usePost(data)
+    }
+    const usePost = () => {
 
-    const {register, handleSubmit, errors} = useForm();     // No se accede por vector, sino que por nombre
-
+    }
     return (
         <>
             <Row className="justify-content-center vh-100 align-items-center m-0 p-0">
             <Col md={3} sm={12}>
-                <Form >
+                <Form onSubmit={handleSubmit(submitForm)}> 
                 <Form.Group>
                     <Form.Control
                     type="text"
                     name="username"
+                    ref={register} // Sirve para tener este input en cuenta
                     />
-                    <label className="text-danger"></label>
+                    {erros.mail && (
+                    <label className="text-danger">{errors.mail.message}</label>
+                    )}
                 </Form.Group>
                 <Form.Group>
                     <Form.Control
                     type="password"
-            
+ // Sirve para tener este input en cuenta
                     name="password"
                     
                     placeholder="password"
