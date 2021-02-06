9fb690195a9ef828fac3ca1b785bfc6c394e9270
Alejogb1 <alejogarciag1@gmail.com>
2021-02-06 18:21:44 -0300

    Update useHttp.js

--- myapp/src/customHooks/useHttp.js
+++ myapp/src/customHooks/useHttp.js
@@ -2,5 +2,16 @@ import {useState} from "react"
 import axios from "axios" // PETICIONES HTTP
 
 export const usePost = () => {
+    const [response, setResponse] = useState(null)
+    const [fetching, setFetching] = useState(false)
 
+    const postData = async (endopoin, object) => {
+        try {
+            setFetching(true)
+            const responseData = await axios.post(`${BASE_URL}/${endpoint}`, object)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    return [postData, response, fetching]
 }
